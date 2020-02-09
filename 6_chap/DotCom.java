63d3a4a0acb23bb2e7d79817fedfc3317e6275ef
trungdv <trungdv60@gmail.com>
2020-02-09 11:31:33 +0700

    Chap 6: Original Simple game
    Signed-off-by: trungdv <trungdv60@gmail.com>

---
+++ 6_chap/DotCom.java
@@ -0,0 +1,27 @@
+public class DotCom {
+	int[] locationsCells;
+	int numOfHits = 0;
+	
+	public void setLocationCells(int[] locs) {
+		locationsCells = locs;
+	}
+	
+	public String checkYourself (String stringGuess) {
+		int guess = Integer.parseInt(stringGuess);
+		String result = "miss";
+		for (int cell : locationsCells) {
+			if (guess == cell) {
+				result = "hit";
+				numOfHits++;
+				break;
+			}
+		} // out of loop
+		
+		if (numOfHits == locationsCells.length) {
+			result = "kill";
+		}
+		
+		System.out.println(result);
+		return result;
+	} // close method
+} // close class
